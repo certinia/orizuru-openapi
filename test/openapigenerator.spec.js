067e8e2e51305b5f359c3943f68cc3dfca460201
Stephen Fry <sfry@financialforce.com>
2017-10-13 13:46:22 +0100

    Add swagger file for none test.

--- test/openapigenerator.spec.js
+++ test/openapigenerator.spec.js
@@ -75,23 +75,7 @@ describe('openapigenerator.js', () => {
 			// then
 
 			calledOnce(res.json);
-			calledWith(res.json, {
-				swagger: '2.0',
-				info: {
-					version: '1.0.0',
-					title: 'Test',
-					description: 'Desc'
-				},
-				host: 'test.com',
-				basePath: '/api',
-				schemes: [
-					'http'
-				],
-				consumes: ['application/json'],
-				produces: ['application/json'],
-				paths: {},
-				definitions: {}
-			});
+			calledWith(res.json, require('./resources/swagger/none'));
 
 		});
 
